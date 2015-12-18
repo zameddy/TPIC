2cc878b76e17713047fb2b9790b920e740c892a7
zameddy <eddy.zam@gmail.com>
2015-12-18 15:01:33 +0100

    création Class PersonneDao

---
+++ monappli-dao/src/main/java/com/objis/demomaven/PersonneDao.java
@@ -0,0 +1,32 @@
+package com.objis.demomaven;
+
+import java.util.List;
+
+public class PersonneDao implements IPersonne {
+
+	public void savePersonne(Personne p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public List<Personne> listPersonne() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void deletePersonne(Personne p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Personne findPersonne(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Personne updatePersonne(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
