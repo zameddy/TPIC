f405cbe85d6614377835ef3b655e0a242b87bc47
zameddy <eddy.zam@gmail.com>
2015-12-18 14:43:13 +0100

    création de la classe personne

---
+++ monappli-domaine/src/main/java/com/objis/demomaven/Personne.java
@@ -0,0 +1,33 @@
+package com.objis.demomaven;
+
+public class Personne {
+
+	private long id;
+	private String nom;
+	private String prenom;
+	
+	public Personne() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	
+}
